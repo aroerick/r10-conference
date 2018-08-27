45ed33e16355e4a8b681ffa95f3e29fee0d424e2
aroerick <a.roerick@gmail.com>
2018-08-27 08:38:14 -0700

    StatusBar to white, minor padding fix on about container

--- js/App.js
+++ js/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import { StatusBar } from 'react-native'
 import client from './config/api'
 import { ApolloProvider } from 'react-apollo'
 import RootStackNavigator from './navigation/RootStackNavigation'
 import { FavsProvider } from './context/FavsContext'
-
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <FavsProvider>
+          <StatusBar barStyle="light-content" />
           <RootStackNavigator />
         </FavsProvider>
       </ApolloProvider>
--- js/screens/About/styles.js
+++ js/screens/About/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    padding: 12,
+    paddingHorizontal: 12,
   },
   image: {
     alignSelf: 'center',
